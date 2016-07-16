b20dce22ba95b6679126fc1f3d09ec5202459192
Colin Kingen <colinwkingen@gmail.com>
2016-07-15 17:04:16 -0700

    create several integration specs to doublecheck functionality

--- spec/client_integration_spec.rb
+++ spec/client_integration_spec.rb
@@ -1,17 +1,42 @@
-# require("capybara/rspec")
-# require("./app")
-# require('spec_helper')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('salon routing', {:type => :feature}) do
-#   before() do
-#     Client.clear()
-#   end
-#   it('accepts a new client entry') do
-#     visit('/')
-#     fill_in('new_client', :with => 'Martin')
-#     click_button('Add Client')
-#     expect(page).to have_content('Martin')
-#   end
-# end
+require("capybara/rspec")
+require("./app")
+require('spec_helper')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('salon routing', {:type => :feature}) do
+  before() do
+    Client.clear()
+  end
+  it('accepts a new client entry') do
+    visit('/')
+    fill_in('new_stylist', :with => 'Sandra')
+    click_button('Add')
+    click_button('View Clients')
+    fill_in('new_client', :with => 'Lucy')
+    click_button('Add')
+    expect(page).to have_content('Lucy')
+  end
+  it('removes a client entry') do
+    visit('/')
+    fill_in('new_stylist', :with => 'Sandra')
+    click_button('Add')
+    click_button('View Clients')
+    fill_in('new_client', :with => 'Lucy')
+    click_button('Add')
+    click_button('Remove Booking')
+    expect(page).to_not have_content('Lucy')
+  end
+  it('alters a booking name') do
+    visit('/')
+    fill_in('new_stylist', :with => 'Sandra')
+    click_button('Add')
+    click_button('View Clients')
+    fill_in('new_client', :with => 'Lucy')
+    click_button('Add')
+    fill_in('client_name', :with => 'Tom')
+    click_button('Update')
+    expect(page).to_not have_content('Lucy')
+    expect(page).to have_content('Tom')
+  end
+end
--- spec/stylist_integration_spec.rb
+++ spec/stylist_integration_spec.rb
@@ -1,17 +1,34 @@
-# require("capybara/rspec")
-# require("./app")
-# require('spec_helper')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('word define routing', {:type => :feature}) do
-#   before() do
-#     Stylist.clear()
-#   end
-#   it('accepts a new stylist entry') do
-#     visit('/')
-#     fill_in('new_stylist', :with => 'Sandra')
-#     click_button('Add Stylist')
-#     expect(page).to have_content('Sandra')
-#   end
-# end
+require("capybara/rspec")
+require("./app")
+require('spec_helper')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('word define routing', {:type => :feature}) do
+  before() do
+    Stylist.clear()
+  end
+  it('accepts a new stylist entry') do
+    visit('/')
+    fill_in('new_stylist', :with => 'Sandra')
+    click_button('Add')
+    expect(page).to have_content('Sandra')
+  end
+  it('removes a stylist entry') do
+    visit('/')
+    fill_in('new_stylist', :with => 'Sandra')
+    click_button('Add')
+    click_button('Send Home')
+    expect(page).to_not have_content('Sandra')
+  end
+  it('changes a stylists name') do
+    visit('/')
+    fill_in('new_stylist', :with => 'Sandra')
+    click_button('Add')
+    click_button('View Clients')
+    fill_in('stylist_name', :with => 'Audry')
+    click_button('Update Name')
+    expect(page).to_not have_content('Sandra')
+    expect(page).to have_content('Audry')  
+  end
+end
