38880f6b7ffd6a19bf3fa44c65ffdfb51db87c4e
Colin Kingen <colinwkingen@gmail.com>
2016-07-18 21:46:12 -0700

    change routing to match RESTful conventions

--- app.rb
+++ app.rb
@@ -12,19 +12,19 @@ get('/') do
   @all_clients = Client.all()
   erb(:index)
 end
-post('/add_stylist') do
+post('/stylists') do
   @newest_stylist = params.fetch('new_stylist')
   Stylist.new({:name => @newest_stylist}).save()
   @all_stylists = Stylist.all()
   @all_clients = Client.all()
   erb(:index)
 end
-get('/add_stylist') do
+get('/stylists') do
   @all_stylists = Stylist.all()
   @all_clients = Client.all()
   erb(:index)
 end
-post('/add_client') do
+post('/clients') do
   @newest_client = params.fetch('new_client')
   @chosen_stylist = params.fetch('chosen_stylist').to_i
   @this_stylist = Stylist.find(@chosen_stylist)
@@ -34,36 +34,36 @@ post('/add_client') do
   @all_clients = Client.all()
   erb(:add_clients)
 end
-get('/add_client') do
+get('/clients') do
   @this_stylist = Stylist.find(@chosen_stylist)
   @has_clients = Client.find_by_stylist(@this_stylist.id.to_i)
   @all_stylists = Stylist.all()
   @all_clients = Client.all()
   erb(:add_clients)
 end
-post('/one_stylist') do
+get('/stylists/:id') do
   @this_stylist_id = params.fetch('these_clients').to_i
   @has_clients = Client.find_by_stylist(@this_stylist_id)
   @this_stylist = Stylist.find(@this_stylist_id)
   erb(:add_clients)
 end
-delete('/remove_client') do
+delete('/clients/:id') do
   @client = Client.find(params.fetch('chosen_client').to_i)
   @client.delete()
   redirect to('/')
 end
-delete('/remove_stylist') do
+delete('/stylists/:id') do
   @stylist = Stylist.find(params.fetch('chosen_stylist').to_i)
   @stylist.delete()
   redirect to('/')
 end
-patch('/change_client_name') do
+patch('/clients/:id') do
   @client = Client.find(params.fetch('chosen_client').to_i)
   name = params.fetch('client_name')
   @client.update_name(name)
   redirect to('/')
 end
-patch('/change_stylist_name') do
+patch('/stylists/:id') do
   @stylist = Stylist.find(params.fetch('chosen_stylist').to_i)
   name = params.fetch('stylist_name')
   @stylist.update_name(name)
